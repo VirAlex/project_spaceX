083e2e7c22a0893bc53c9205de0b1ef2810de0c3
VirAlex <alexou73@gmail.com>
2020-02-06 16:46:53 +0100

    error public

---
+++ public/precache-manifest.8fb47c01ee023e47a3bd7b35c2e301a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bb7146b50656be725833451a6f2985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218a39b1743effd305d0",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c8544242f9e8f77124a3",
+    "url": "/static/js/2.817f6e2b.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.817f6e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218a39b1743effd305d0",
+    "url": "/static/js/main.dd019492.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.png"
+  }
+]);
